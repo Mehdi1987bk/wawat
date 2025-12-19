d94a4e3177046a9f5bfdb3ba6bf7123821c0f0c0
Mehdi1987bk <mehdi1987@bk.ru>
2025-12-19 16:57:18 +0400

    add icon launch

--- android/app/src/main/kotlin/az/buking/buking/MainActivity.kt
+++ android/app/src/main/kotlin/az/buking/buking/MainActivity.kt
@@ -1,5 +1,16 @@
-package az.buking.buking
+package com.app.mhm
 
+import android.os.Build
+import android.os.Bundle
+import androidx.core.view.WindowCompat
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            WindowCompat.setDecorFitsSystemWindows(window, false)
+        }
+    }
+}
